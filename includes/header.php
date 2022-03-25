a0ee39cba21493b7719c2f9b70a930a6a306b74b
Jade Mark <88232304+jd022@users.noreply.github.com>
2022-03-26 07:47:18 +0800

    initial commit (stepper)

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="src/css/bootstrap.css">
     <link rel="stylesheet" href="src/css/custom-style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <title>PDMES</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
@@ -17,13 +19,13 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item mx-3">
-                <a class="nav-link mx-3" href="index.php">Home</a>
+            <li class="nav-item mx-1">
+                <a class="nav-link mx-4" href="index.php">Home</a>
             </li>
-            <li class="nav-item mx-3">
+            <li class="nav-item mx-4">
                 <a class="nav-link mx-2" href="#">History Log</a>
             </li>
-            <li class="nav-item mx-3">
+            <li class="nav-item">
                 <a class="nav-link mx-3" href="logout.php">Log out</a>
             </li>
         </ul>
